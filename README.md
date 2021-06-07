a304ffeed124c8ca7335fffde80a30ae1d67c4bc
Uriel Nunez <uanq22@hotmail.com>
2021-06-07 00:31:49 -0500

    READ ME gets generated after answering prompted questions

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# README-Generator
+# s
+
+## Description
+s
+
+## Table of Contents
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [License](#License)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+
+## Installation
+s
+    
+## Usage
+s
+
+## License
+MIT
+
+## Contributing
+s
+
+## Tests
+s
+
+## Questions
+If you have any questions please contact me via GitHub at s] or via email at s.
--- index.js
+++ index.js
@@ -2,61 +2,86 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const generateMD = (answers) => {
+  return `# ${answers.name}
 
-  `##${answers.name}
-  *${answers.motivation}
-  *${answers.reason}
-  *${answers.solution}
-  *${answers.learn}
-  *${answers.license}
-  *${answers.github}
-  *${answers.email}`
-}
+## Description
+${answers.description}
+
+## Table of Contents
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [License](#License)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+
+## Installation
+${answers.installation}
+    
+## Usage
+${answers.usage}
+
+## License
+${answers.license}
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+If you have any questions please contact me via GitHub at ${answers.github}] or via email at ${answers.email}.`
+};
 
 inquirer
   .prompt([
     {
       type: 'input',
       name: 'name',
-      message: 'What is the name of your project?',
+      message: 'What is the name of your project.',
     },
     {
       type: 'input',
-      name: 'motivation',
-      message: 'What was your motivation?',
+      name: 'description',
+      message: 'Describe what was your motivation and reason for this project',
     },
     {
       type: 'input',
-      name: 'reason',
-      message: 'Why did you build this project?',
+      name: 'installation',
+      message: 'What are the steps required to install your project?'
     },
     {
       type: 'input',
-      name: 'solution',
-      message: 'What problem does it solve?',
-    },
-    {
-      type: 'input',
-      name: 'learn',
-      message: 'What did you learn?',
+      name: 'usage',
+      message: 'Provide instructions and examples for use. Include screenshots as needed.'
     },
     {
       type: 'list',
       name: 'license',
-      message: 'What license did you use?',
-      choices: ['MIT', 'GPL', 'Apache', 'none'],
+      message: 'Choose a license.',
+      choices: ['MIT', 'GPL', 'Apache', 'none']
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'If you would like other developers to contribute to your application or package, include guidelines here.'
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Does your application need any tests.'
     },
     {
       type: 'input',
       name: 'github',
-      message: 'What is your GitHub username?',
+      message: 'Enter your GitHub user name.'
     },
     {
       type: 'input',
       name: 'email',
-      message: 'What is your E-Mail?',
+      message: 'Enter your email address.'
     },
-
   ])
   .then((answers) => {
     const readMeContent = generateMD(answers);
